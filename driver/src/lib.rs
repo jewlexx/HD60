6d58913cca1f45b4faff36ad92ce4ca1ebb0c5e8
James Cordor <grassojames5@gmail.com>
2022-02-18 13:12:58 +1100

    AHHHH

--- driver/src/lib.rs
+++ driver/src/lib.rs
@@ -1,11 +1,11 @@
 #![no_std]
 
-use core::panic::PanicInfo;
-
 #[no_mangle]
 pub extern "system" fn driver_entry() -> u32 {
-    0 /* STATUS_SUCCESS */
+    0 /* NT_STATUS_SUCCESS */
 }
+
+use core::panic::PanicInfo;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
